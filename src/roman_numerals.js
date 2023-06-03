45300772a73308a6683d26834e4fbefe7a2058cb
Brian <brian@vvgonline.com>
2023-06-03 17:35:01 +0100

    refactor to remove some unnecessary variables

--- src/roman_numerals.js
+++ src/roman_numerals.js
@@ -1,6 +1,7 @@
 export function convertToRoman(number) {
   if (number === undefined || number <= 0)
     throw new Error("Enter an integer greater than 0");
+
   const numberKey = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
   const romanKey = [
     "I",
@@ -19,29 +20,24 @@ export function convertToRoman(number) {
   ];
 
   let x = number;
-  let romanNumber = "";
+  let romanNumeral = "";
   let largestNumber = 0;
 
+  // NTS - not mad about the do..while loop. Consider refactoring.
   do {
     x = x - largestNumber;
-    const numbersLessThan = numberKey.filter((n) => n <= x);
 
-    const isLargeNumber = (n) => n === largestNumber;
-    if (numbersLessThan.length === 0) {
+    const numbersLessThanX = numberKey.filter((n) => n <= x);
+
+    if (numbersLessThanX.length === 0) {
       break;
     }
-    largestNumber = Math.max(...numbersLessThan);
-    const largestNumberIndex = numberKey.findIndex(isLargeNumber);
-    romanNumber = romanNumber + romanKey[largestNumberIndex];
 
-    // console.log("numbersLessThan: " + numbersLessThan);
-    // console.log("largestNumber: " + largestNumber);
-    // console.log("largestNumberIndex:" + largestNumberIndex);
-    // console.log(
-    //   romanNumber + " + " + romanKey[largestNumberIndex] + " = " + romanNumber
-    // );
-    // console.log("x:" + x);
+    largestNumber = Math.max(...numbersLessThanX);
+
+    romanNumeral =
+      romanNumeral + romanKey[numberKey.findIndex((n) => n === largestNumber)];
   } while (x != 1);
 
-  return romanNumber;
+  return romanNumeral;
 }
